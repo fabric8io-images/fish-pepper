452c3114261a01fd856c66862457278a6c60dca6
Roland Huß <roland@ro14nd.de>
2019-04-10 09:38:53 +0200

    minor fix

--- docker/build.sh
+++ docker/build.sh
@@ -2,4 +2,4 @@
 
 version=$(grep "ENV FISH_PEPPER_VERSION" Dockerfile | awk '{ print $3 }')
 docker build -t fabric8/fish-pepper:$version .
-docker tag -f fabric8/fish-pepper:$version fabric8/fish-pepper:latest
+docker tag fabric8/fish-pepper:$version fabric8/fish-pepper:latest
